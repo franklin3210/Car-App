19da50f46499a26e0a94c3e35a04f3ae54570d3a
franklin3210 <rajfranklin65@gmail.com>
2024-08-28 15:37:06 +0530

    searchbar enable and disable

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,37 +1,43 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import SearchBar from "../SearchBar"
+import { useNavigate, useLocation } from 'react-router-dom';
+import { FaSearch } from 'react-icons/fa'; // Import search icon from react-icons
+
 import carlogo from "../../carlogo.png";
 import './index.css'; // Ensure this path is correct
 
 function Navbar({ setSearchTerm }) {
   const navigate = useNavigate();
+  const location = useLocation();
 
-//   const handleSearchChange = (e) => {
-//     setSearchTerm(e.target.value);
-//   };
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
 
   const handleLogoClick = () => {
     navigate('/');
   };
+
+  // Check if the current path matches /car/:id
+  const isCarDetailPage = /^\/car\/\d+$/.test(location.pathname);
 
   return (
     <div className="nav-wrapper">
       <div className="nav-class">
         <div className="nav-left" onClick={handleLogoClick} style={{ cursor: 'pointer' }}>
           <img src={carlogo} alt="Logo" className="car-logo" />
-          <h1 className="car-list-heading">Car List</h1>
+          <h1 className="car-list-heading">motogarage</h1>
         </div>
-        <div className="search-bar-container">
-          
-          {/* <input
-            type="text"
-            className="search-bar"
-            onChange={handleSearchChange}
-            placeholder="Search for cars..."
-          /> */}
-          <SearchBar setSearchTerm={setSearchTerm} />
-        </div>
+        {!isCarDetailPage && (
+          <div className="search-bar-container">
+            <FaSearch className="search-icon" />
+            <input
+              type="text"
+              className="search-bar"
+              onChange={handleSearchChange}
+              placeholder="Search for cars..."
+            />
+          </div>
+        )}
       </div>
     </div>
   );
